5f0fbd7e14307d16101694b094828bcfb85e503f
Georg Popp <Georg.Popp@outlook.com>
2024-11-05 11:24:02 +0100

    feat: setting up fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'Helvetica Neue': require('../assets/fonts/HelveticaNeueMedium.otf'),
+    'Helvetica Neue Bold': require('../assets/fonts/HelveticaNeueBold.otf'),
+    'Helvetica Neue Black': require('../assets/fonts/HelveticaNeueBlack.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) return null;
+
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name='index' />
     </Stack>
   );
 }
