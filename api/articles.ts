a9a10c9e519e65caf7fc87427334c8d8ee748935
Georg Popp <Georg.Popp@outlook.com>
2024-11-06 09:58:13 +0100

    refactor: remove simulated delay in getArticles function

--- api/articles.ts
+++ api/articles.ts
@@ -1,9 +1,6 @@
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const getArticles = async () => {
-  // Simulate a delay
-  await new Promise((resolve) => setTimeout(resolve, 800));
-
   const response = await fetch(`${API_URL}/articles`);
   return await response.json();
 };
